b60d936fd7c9e6f631748656779dfbc80271c9df
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-05-22 14:10:16 -0400

    this should be all the right files

---
+++ get_jcn.pl
@@ -0,0 +1,20 @@
+use lib '/Users/buchanan/perl5/lib/perl5/';
+
+use WordNet::Similarity::jcn;
+
+use WordNet::QueryData;
+
+$data[0] = "bubble#n#1";
+$data[1] = "gum#n#1";
+
+my $wn = WordNet::QueryData->new();
+
+my $rel = WordNet::Similarity::jcn->new($wn);
+
+my $value = $rel->getRelatedness($data[0], $data[1]);
+
+($error, $errorString) = $rel->getError();
+
+if($error) { print "$data[0] $data[1] $errorString\n";}
+
+else {print "$data[0] <-> $data[1] = $value\n";}
