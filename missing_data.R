37419b38ef8db2e2b4fedb2bd09e5990784efe3a
Ari Cunningham <arielle924@gmail.com>
2018-09-25 12:42:26 -0500

    Added code to find missing data

--- missing_data.R
+++ missing_data.R
@@ -19,9 +19,40 @@ master$prime[is.na(master$p.orthoN)]
 master$target[is.na(master$t.orthoN)]
 
 
+# phonoN ------------------------------------------------------------------
+master$target[is.na(master$p.phonoN)]
+master$target[is.na(master$t.phonoN)]
+
+
+# freq --------------------------------------------------------------------
+
+master$target[is.na(master$p.freq)]
+master$target[is.na(master$t.freq)]
+
+
 # swow fsg and pmi ----------------------------------------------------------------
 
 master$index[is.na(master$swowfsg)]
+master$index[is.na(master$swow.t.fsg_ss)]
+master$index[is.na(master$swow.p.fsg_ss)]
+master$index[is.na(master$pmi_swow)]
+
+
+# swow fan ----------------------------------------------------------------
+
+master$index[is.na(master$swow.t.fan_ss)]
+master$index[is.na(master$swow.p.fan_ss)]
+
+
+# fan_ss ------------------------------------------------------------------
+
+master$index[is.na(master$p.fan_ss)]
+
+
+# POSr --------------------------------------------------------------------
+
+master$index[is.na(master$p.POSr)]
+master$index[is.na(master$t.POSr)]
 
 #dont: POS, anything fsg/bsg that doesn't say swow, JCN, root, affix, LSA, LSA2, SOA
 
