2fe67360178d36d39d5fd5eaa8b857dee84e7ed9
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-10-14 19:33:23 -0500

    creating example

--- subject_soa1200_first_name_erin.R
+++ subject_soa1200_first_name_erin.R
@@ -3,3 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #use the right file either LDT or N
 spp.data = read.csv("subjectdataN.csv") 
+
+#pull only the information you are interested in
+spp.data = subset(spp.data, 
+                  )
